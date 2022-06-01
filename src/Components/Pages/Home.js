c2602af715d0095538e7034163a65e03c7c79051
Anuraj <anurajam13@gmail.com>
2022-06-01 22:30:05 +0530

    home page done

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,11 +1,29 @@
 import React from 'react'
+import {Link}from 'react-router-dom'
+import insta from './insta.png'
 
 function Home() {
   return (
     <div className='container2'>
-      <h1>Home</h1>
+     	<div className='content'>
+					<h6>Welcome . . .</h6>
+					<h2>WhiteLights</h2>
+					<h5>Photography - Videography</h5>
+					
+					<ul className='dis' >
+						<li  className='instagram' ><a href="https://www.instagram.com/whitelightsfilms/"><img  className='insta-icon' src={insta}></img></a></li>
+						
+			        </ul>
+					<Link to='/gallery'className='btn1' >Gallery </Link>
+					<Link to='/contact' className='btn2'>Contact </Link>
+					
+				</div>
+      
+      
+      
          
     </div>
+  
   )
 }
 
